ff99178c5dcbb4a0adc5ff8c0d0088de2e0a1581
authdev2 <flaviogomes05.work@gmail.com>
2025-08-18 15:08:11 +0100

    Novas funcionalides na api para o site de receitas

--- api/server.js
+++ api/server.js
@@ -1,30 +1,31 @@
 import express from "express";
+import fs from "fs";
+
 const app = express();
 
 const PORT = 3000;
 app.use(express.json());
 
-let receitas = [
-    {
-        id: 1,
-        nome: "Pizza",
-        ingredientes: ["farinha", "água", "fermento", "sal", "azeitona", "queijo", "molho de tomate"],
-        modoPreparo: "Misturar todos os ingredientes e assar em forno a 200°C por 30 minutos.",
-        tempoPreparo: 30,
-        rendimento: 8,
-        categoria: "Fast Food",
-    }
-];
+let receitas = JSON.parse(fs.readFileSync("receitas.json", "utf8"));
 
 app.get("/receitas", (req, res) => {
     res.json(receitas);
 });
 
-app.post("/receitas", (req, res) => {
-    const receita = req.body;
-    receitas.push(receita);
-    res.status(201).json(receita);
-});
+app.get("/receitas/random", (req, res) => {
+    let randomIndex = Math.floor(Math.random() * receitas.length);
+    res.json(receitas[randomIndex]);
+})
+
+app.get("/receitas/:categoria", (req, res) => {
+    let receitasFiltradas = receitas.filter(receita => receita.categoria === req.params.categoria);
+    res.json(receitasFiltradas);
+})
+
+app.get("/receitas/searchCompleta/:nome", (req, res) => {
+    let receitasFiltradas = receitas.filter(receita => receita.nome.toLowerCase().includes(req.params.nome.toLowerCase()));
+    res.json(receitasFiltradas);
+})
 
 app.listen(PORT, () => {
     console.log(`Aberto na porta ${PORT}`);
