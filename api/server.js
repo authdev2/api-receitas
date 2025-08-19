2ae3f57f09e7fff5ff0b27644f2a14214e406d48
authdev2 <flaviogomes05.work@gmail.com>
2025-08-19 02:26:44 +0100

    Testes da api

--- api/server.js
+++ api/server.js
@@ -1,13 +1,44 @@
 import express from "express";
 import fs from "fs";
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 const app = express();
-
 const PORT = 3000;
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+
+const receitaSchema = new mongoose.Schema({
+    nome: String,
+    categoria: String,
+    ingredientes: [String],
+    instrucoes: String,
+})
+const connectDb = async () => {
+   await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Conectado ao MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+connectDb();
 
 let receitas = JSON.parse(fs.readFileSync("receitas.json", "utf8"));
+
+app.post("/receitas", (req, res) => {
+    const receita = new receitaSchema({
+        nome: req.body.nome,
+        categoria: req.body.categoria,
+        ingredientes: req.body.ingredientes,
+        instrucoes: req.body.instrucoes,
+    })
+    receita.save();
+    res.json(receita);
+})
 
 app.get("/receitas", (req, res) => {
     res.json(receitas);
